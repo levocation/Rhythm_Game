8e6f13655cd9429755aa2ee472929d6f749bd823
QUOTIENT <tjsgh5965@naver.com>
2023-07-10 15:51:28 +0900

    #02

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,6 +1,9 @@
 package Game;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.*;
 
@@ -10,8 +13,21 @@ public class Game extends JFrame {
 	private Graphics screenGraphic;
 
 	// 이미지 가져오기
-	private Image introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
 	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.jpg")));
+
+	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/exitButtonBasic.png"));
+	private ImageIcon startButtonBasicImage = new ImageIcon(Main.class.getResource("../images/startButtonBasic.png"));
+	private ImageIcon quitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/quitButtonBasic.png"));
+
+	private JButton exitButton = new JButton(exitButtonBasicImage);
+	private JButton startButton = new JButton(startButtonBasicImage);
+	private JButton quitButton = new JButton(quitButtonBasicImage);
+
+
+	private Image background = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
+	
+	
+	private int mouseX, mouseY;
 	
 	public Game() {
 		setUndecorated(true); // 메뉴바가 안보이게 됨
@@ -21,6 +37,94 @@ public class Game extends JFrame {
 		setLocationRelativeTo(null); // 창을 화면의 가운데에 띄움
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 윈도우창 종료시 프로세스까지 닫을 수 있음
 		setVisible(true);
+		setBackground(new Color(0, 0, 0, 0));
+		setLayout(null);
+		
+		menuBar.setBounds(0, 0, 1280, 30);
+		menuBar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+		});
+		menuBar.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				setLocation(x - mouseX, y - mouseY);
+			}
+		});
+		
+		// exitButton
+		exitButton.setBounds(1245, 0, 30, 30);
+		exitButton.setBorderPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			public void mouseExited(MouseEvent e) {
+				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+				System.exit(0);
+			}
+		});
+		add(exitButton);
+		
+		// startButton
+		startButton.setBounds(800, 200, 400, 100);
+		startButton.setBorderPainted(false);
+		startButton.setContentAreaFilled(false);
+		startButton.setFocusPainted(false);
+		startButton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			public void mouseExited(MouseEvent e) {
+				startButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				startButton.setVisible(false);
+				quitButton.setVisible(false);
+				background = new ImageIcon(Main.class.getResource("../images/mainBackground.jpg")).getImage();
+			}
+		});
+		add(startButton);
+		
+		// quitButton
+		quitButton.setBounds(800, 330, 400, 100);
+		quitButton.setBorderPainted(false);
+		quitButton.setContentAreaFilled(false);
+		quitButton.setFocusPainted(false);
+		quitButton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				quitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			public void mouseExited(MouseEvent e) {
+				quitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+				System.exit(0);
+			}
+		});
+		add(quitButton);
+		
+		
+		add(menuBar);
 		
 		Music introMusic = new Music("sangil_song.mp3", true);
 		introMusic.start();
@@ -36,7 +140,7 @@ public class Game extends JFrame {
 	}
 
 	private void screenDraw(Graphics g) {
-		g.drawImage(introBackground, 0, 0, null); // 자주 바뀔 수 있는 이미지를 그리기 (0,0부터)
+		g.drawImage(background, 0, 0, null); // 자주 바뀔 수 있는 이미지를 그리기 (0,0부터)
 		paintComponents(g); // 항상 고정되는 이미지를 그리기
 		this.repaint();
 	}
